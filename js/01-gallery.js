ecf489a0301ca4385cfba486c4a946d3ad666fff
morysol <cepera.cMupHoB@gmail.com>
2022-04-17 19:31:50 +0300

    galleru 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="'${original}'"  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt=""${description}"
+    />
+  </a>
+</div>
+    `;
+  })
+  .join('');
+
+const gallery = document.querySelector('div.gallery');
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsMarkUp);
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  e.currentTarget.onclick = () => {
+    basicLightbox.create(`<img  src="${e.target.getAttribute('data-source')}">`).show();
+  };
+
+  e.currentTarget.removeAttribute('onclick');
+});

c7249262b17db9c493e5dae25520bbceab4c89bd
morysol <cepera.cMupHoB@gmail.com>
2022-04-18 14:18:19 +0300

    gallery 2 final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,12 +4,12 @@ const galleryItemsMarkUp = galleryItems
   .map(({ preview, original, description }) => {
     return `
 <div class="gallery__item">
-  <a class="gallery__link" href="'${original}'"  >
+  <a class="gallery__link" href="${original}"  >
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
-      alt=""${description}"
+      alt="${description}"
     />
   </a>
 </div>
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryLightboxMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}"
+    alt="${description}"
+    title="${description}" />
+    </a></li>
+    `;
+  })
+  .join('');
+
+const gallery = document.querySelector('ul.gallery');
+
+gallery.insertAdjacentHTML('beforeend', galleryLightboxMarkUp);
+const galleryLightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+gallery.addEventListener('click', e => {
+  galleryLightbox.on('show.simplelightbox', function () {
+    // do something…
+  });
+
+  galleryLightbox.close();
+  gallery.removeEventListener('click', e);
+});
